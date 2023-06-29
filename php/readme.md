71bb29287f1194be7f514420c2096a77d1d99e78
Josué Juca <josuejuca19@gmail.com>
2023-06-29 05:29:20 -0300

    Apagando arquivos 🗑️

--- php/readme.md
+++
@@ -1,9 +0,0 @@
-Esse projeto ainda não está finalizado, ficou faltando algumas partes como a de doação de livros e algumas páginas como de dúvidas, e de categorias.
-
-para rodar o projeto você vai precisar preparar o banco de dados com os scripts, logo em seguida baixe essa pasta. 
-
-Abra o terminal e inicie o projeto PHP usando o código:
-
-~~~powershell
-php -S localhost:{porta}
-~~~
--- php/sql/dados.sql
+++
@@ -1,5 +0,0 @@
--- Perfil 
-INSERT INTO `perfil` (`idperfil`, `perfil`) VALUES ('1', 'Administrador'), ('2', 'Funcionario'),('3','Comunidade');
--- Usuarios
-INSERT INTO `usuario` (`idusuario`, `email`, `senha`, `nome`, `dt_nasc`, `cpf`, `rg`, `endereco`, `telefone`, `perfil_idperfil`) VALUES (NULL, 'juca@juca.com', '7b5a67574d8b1d77d2803b24946950f0', 'Juca', '2003-12-19', '000.000.000-00', '00.000.000-0', 'Ceilandia Sul', '(61) 9 8463-4855', '1')
--- 
